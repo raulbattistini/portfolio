1325a8e1282a5e4694cf980826a17f27f521a02c
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-12 16:55:05 -0300

    added: sample text and size

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Footer } from '../../components/Footer'
-import { Navbar } from '../../components/Navbar'
+import React, { useEffect } from "react";
+import { Footer } from "../../components/Footer";
+import { Navbar } from "../../components/Navbar";
 
 export const Home = () => {
+
   return (
-    <div>
-      <Navbar/>
-      <Footer/>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="main">
+        <span className="flex mt-96 text-center text-lg">
+          {" "}
+          pagina principal{" "}
+        </span>
+        <span className="flex mt-96 text-center text-lg">
+          {" "}
+          pagina principal{" "}
+        </span>
+        <span className="flex mt-96 text-center text-lg">
+          {" "}
+          pagina principal{" "}
+        </span>
+        <span className="flex mt-96 text-center text-lg">
+          {" "}
+          pagina principal{" "}
+        </span>
+        <span className="flex mt-96 text-center text-lg">
+          {" "}
+          pagina principal{" "}
+        </span>
+      </div>
+      <Footer />
+    </>
+  );
+};
