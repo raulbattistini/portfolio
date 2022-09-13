12b36fb204b129ef47679403ea0b8e7141756e73
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-13 16:55:30 -0300

    upd: components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,41 +1,27 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import defaultImg from "../../assets/images/default.jpg";
-import facebook from "../../assets/images/icons/facebook.png";
-import instagram from "../../assets/images/icons/instagram.png";
+import githubLogo from "../../assets/images/icons/GitHub-icon.png";
+import linkedin from "../../assets/images/icons/linkedin.png";
 
 export const Footer = () => {
   return (
     <>
-    <span className=" bg-[#272727] text-white text-2xl pb-3 pt-3 align-center text-center self-center justify-center flex">
-      Últimos posts:
+    <span className=" bg-[#272727] text-white text-2xl pb-3 pt-3 align-center text-center self-center justify-center flex divide-x">
+      
     </span>
-      <Grid container className="divide-x">
-        <Grid item xs={3}>
-          <img src={defaultImg} alt="Place image..." />
-        </Grid>
-        <Grid item xs={3}>
-          <img src={defaultImg} alt="" />
-        </Grid>
-        <Grid item xs={3}>
-          <img src={defaultImg} alt="" />
-        </Grid>
-        <Grid item xs={3}>
-          <img src={defaultImg} alt="" />
-        </Grid>
-      </Grid>
-      <div className="bg-[#272727] text-white flex text-2xl justify-center pt-2 pb-2">
+      <div className="bg-[#272727] text-white flex text-2xl justify-center">
         <span className=" pt-8 font-inter font-thin">
           {" "}
           Consulte também minhas redes sociais:{" "}
         </span>
         <img
-          src={facebook}
+          src={githubLogo}
           className="w-10 h-10 flex flex-row justify-between m-8 object-cover"
-          alt="Ícone do Facebook"
+          alt="Ícone do GitHub"
         />
         <img
-          src={instagram}
+          src={linkedin}
           className="w-10 h-10 flex flex-row justify-between m-8"
           alt="Ícone do Instagram"
         />{" "}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useInView, InView } from "react-intersection-observer";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export const Navbar = () => {
   const [ref, inView] = useInView({
@@ -13,7 +14,7 @@ export const Navbar = () => {
   });
 
   return (
-    <div ref={ref} className="ease-in-out	">
+    <div ref={ref} className="ease-in-out	font-thin">
       <div>
         {!inView && (
           <section className="default top-0 sticky middle ">
@@ -22,28 +23,18 @@ export const Navbar = () => {
               className="grid bg-black-rgba font-thin top-0 fixed lazy text-white text-2xl pb-3 pt-3"
             >
               <Grid item xs={4}>
-                <div className="ml-5 flex hover:text-[#4567ff]">
-                  <Link to="/">Raul Battistini</Link>
+                <div className="ml-5 flex">
+                  <Link to="/" className="hover:text-[#4567ff]">Raul Battistini </Link>
                 </div>
               </Grid>
               <Grid item xs={4}>
                 <div className="align-center text-center">
-                  <Grid container>
-                    <Grid item xs={4} className="hover:text-[#4567ff]">
-                      Tecnologias
-                    </Grid>
-                    <Grid item xs={4} className="hover:text-[#4567ff]">
-                      Projetos
-                    </Grid>
-                    <Grid item xs={4} className="hover:text-[#4567ff]">
-                      Experiência
-                    </Grid>
-                  </Grid>
+                 
                 </div>
               </Grid>
               <Grid item xs={4}>
-                <div className="flex flex-end float-right mr-5 hover:text-[#4567ff]">
-                  <Link to="/contact">
+                <div className="flex flex-end float-right mr-5 ">
+                  <Link to="/contact" className="hover:text-[#4567ff]">
                     {" "}
                     Entrar em contato{" "}
                     <ArrowForwardIosIcon className="justify-center" />
@@ -56,24 +47,21 @@ export const Navbar = () => {
         <section className="default top" ref={ref}>
           <Grid
             container
-            className="grid bg-[#272727] font-thin text-white text-2xl pb-3 pt-3"
+            className="grid bg-[#171717] font-thin text-white text-2xl pb-3 pt-3"
           >
             <Grid item xs={4}>
-              <div className="ml-5 flex hover:text-[#4567ff]">
-                <Link to="/">Raul Battistini</Link>
+              <div className="ml-5 flex ">
+                <Link to="/" className="hover:text-[#4567ff]">Raul Battistini </Link>
               </div>
             </Grid>
             <Grid item xs={4}>
               <div className="align-center text-center">
                 <Grid container>
-                  <Grid item xs={4} className="hover:text-[#4567ff]">
-                    Tecnologias
+                  <Grid item xs={6}>
+                  <Link to="/" className="hover:text-[#4567ff]">  Blog</Link>
                   </Grid>
-                  <Grid item xs={4} className="hover:text-[#4567ff]">
-                    Projetos
-                  </Grid>
-                  <Grid item xs={4} className="hover:text-[#4567ff]">
-                    Experiência
+                  <Grid item xs={6}>
+                  <Link to="/" className="hover:text-[#4567ff]">  Certificados</Link>
                   </Grid>
                 </Grid>
               </div>
