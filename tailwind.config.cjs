54e191400d2e37b061746012caf9903025ea88b6
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-13 16:54:50 -0300

    config css files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'poppins': ['poppins, "sans-serif"'],
+      'thin': ['poppins-thin, "sans-serif"'],
+      'light': ['poppins-light, "sans-serif"'],
+      'extralight': ['poppins-extralight, "sans-serif"'],
+      'bold': ['poppins-bold, "sans-serif"'],
+    },
     extend: {
       colors: {
-        'black-rgba': 'rgba(0, 0, 0, 0.54)',
+        'black-rgba': 'rgba(0, 0, 0, 0.5)',
+        'transparet': 'rgba(255, 255, 255, 0)',
+        'glass-white': 'rgba(255, 255, 255, 0.1)'
       }},
   },
   plugins: [],
