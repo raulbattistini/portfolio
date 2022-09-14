5cdec19a17dd7c50996af46b3bfbbf06d69a6064
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-14 16:41:06 -0300

    upd: config files w/ keyframe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      'poppins': ['poppins, "sans-serif"'],
-      'thin': ['poppins-thin, "sans-serif"'],
-      'light': ['poppins-light, "sans-serif"'],
-      'extralight': ['poppins-extralight, "sans-serif"'],
-      'bold': ['poppins-bold, "sans-serif"'],
+      poppins: ['poppins, "sans-serif"'],
+      thin: ['poppins-thin, "sans-serif"'],
+      light: ['poppins-light, "sans-serif"'],
+      extralight: ['poppins-extralight, "sans-serif"'],
+      bold: ['poppins-bold, "sans-serif"'],
     },
     extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
       colors: {
-        'black-rgba': 'rgba(0, 0, 0, 0.5)',
-        'transparet': 'rgba(255, 255, 255, 0)',
-        'glass-white': 'rgba(255, 255, 255, 0.1)'
-      }},
+        "black-rgba": "rgba(0, 0, 0, 0.5)",
+        transparent: "rgba(255, 255, 255, 0)",
+        "glass-white": "rgba(255, 255, 255, 0.1)",
+      },
+    },
   },
   plugins: [],
-}
+};
