3d28ea515865091e131f62cb98be06b378ee1874
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-16 13:01:31 -0300

    add: drawer for responsive navbar

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import defaultImg from "../../assets/images/default.jpg";
+import React, {useState} from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import Avatar from "@mui/material/Avatar";
 import MenuItem from "@mui/material/MenuItem";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 export const Drawer = () => {
-  const clearCache = () => {
-    window.location.reload();
-  };
+  const [selected, setSelected] = useState(false);
 
+  const [open, setOpen] = useState(false);
+
+ 
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
@@ -28,58 +26,60 @@ export const Drawer = () => {
 
   return (
     <>
-      <Box sx={{ flexGrow: 0 }}>
-        <IconButton
-          onClick={handleOpenUserMenu}
-          sx={{ p: 0, justifyContent: "center" }}
-        >
-          <Avatar alt="Remy Sharp" src={defaultImg} className="test" />
-        </IconButton>
-        <IconButton
-          onClick={handleOpenUserMenu}
-          sx={{ p: 0, justifyContent: "center" }}
-        >
-          <MenuIcon className="hd" />
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
+    <Box sx={{ flexGrow: 0 }} className="text-black">
+      <IconButton
+        onClick={handleOpenUserMenu}
+        sx={{ p: 0, justifyContent: "center" }}
+      >
+        <button
+          onClick={() => {
+            setSelected(!selected);
           }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
         >
-          <MenuItem>
-            <button>
-              {" "}
-              <Link
-                to="/blog"
-                className="text-white"
-                style={{ textDecoration: "none", color: "#fff" }}
-              >
-                Blog
-              </Link>
-            </button>
-          </MenuItem>
-          <MenuItem>
-            <Typography
-              textAlign="center"
-              onClick={clearCache}
-              sx={{ color: "#6D70E0" }}
+          <AiOutlineMenuUnfold className="ultrawide:hidden desktop:hidden laptop:hidden tablet:hidden phone:hidden pocket:text-white cursor-pointer" />
+        </button>
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem>
+          <button>
+            {" "}
+            <Link
+              to="/blog"
+              className="text-white"
+              style={{ textDecoration: "none", color: "black" }}
             >
-              {" "}
-              Sair{" "}
-            </Typography>
-          </MenuItem>
-        </Menu>
-      </Box>
-    </>
+              Blog
+            </Link>
+          </button>
+        </MenuItem>
+        <MenuItem>
+          <button>
+            {" "}
+            <Link
+              to="/certificados"
+              className="text-white"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Certificados
+            </Link>
+          </button>
+        </MenuItem>
+      </Menu>
+    </Box>
+  </>
   );
 };
