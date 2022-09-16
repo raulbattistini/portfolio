0f572d35dba2e22ed2704043b125cb945f469a6e
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-16 13:02:15 -0300

    upd: responsive blog page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -29,8 +29,8 @@ export const Blog = () => {
       </div>
       <div className="pt-10 ">
         <Grid container className="pb-10">
-          <Grid item xs={3}>
-            <FaCss3Alt className="w-40 h-28 object-cover ml-8 rounded-md mb-3 relative text-white" />
+          <Grid item xs={3} className="tablet:pt-10  phone:pt-12">
+            <FaCss3Alt className="w-40 h-28 object-cover ml-8 rounded-md mb-3 relative text-white tablet:w-28 tablet:h-16 phone:w-28 phone:h-16 pocket:w-20 pocket:h-12 tablet:ml-0 phone:ml-0 pocket:ml-0 pocket:mt-12" />
           </Grid>
           <Grid item xs={9}>
             <div className="text-white flex flex-col justify-between pt-5 relative ">
@@ -38,11 +38,11 @@ export const Blog = () => {
                 to="https://raulbattistini.hashnode.dev/series/css-options"
                 className="font-thin"
               >
-                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-sm hover:text-yellow-500">
+                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-l-sm hover:text-yellow-500">
                   Estilizando projetos
                 </h4>
               </Link>
-              <span className="font-extralight">
+              <span className="font-extralight pocket:text-sm">
                 Por ser algo que todos têm contato em algum momento, decidi
                 falar sobre CSS e algumas alternativas que existem,
                 principalmente para quem se identifica comigo e não gosta de
@@ -50,9 +50,9 @@ export const Blog = () => {
               </span>
             </div>
           </Grid>
-          <Grid item xs={3}>
-            <SiTypescript className="justify-center flex w-16 h-12 object-cover ml-12 mb-3 relative text-white" />
-            <SiJavascript className="w-16 h-12 object-cover mb-3 text-white rounded-lg ml-24" />
+          <Grid item xs={3} className="pt-5 pocket:pt-12">
+            <SiTypescript className="justify-center flex w-16 h-12 object-cover ml-12 mb-3 relative text-white tablet:text-lg phone:text-lg pocket:text-lg tablet:ml-3 phone:ml-3 pocket:ml-0" />
+            <SiJavascript className="w-16 h-12 object-cover mb-3 text-white rounded-lg ml-24 tablet:text-lg phone:text-lg pocket:text-lg tablet:ml-8 phone:ml-8 pocket:ml-2" />
           </Grid>
           <Grid item xs={9}>
             <div className="text-white flex flex-col justify-between pt-5 relative">
@@ -60,19 +60,19 @@ export const Blog = () => {
                 to="https://raulbattistini.hashnode.dev/series/typescript-javascript"
                 className="font-thin"
               >
-                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-sm hover:text-yellow-500">
+                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-l-sm hover:text-yellow-500">
                   Typescript ou Javascript?
                 </h4>{" "}
               </Link>
-              <span className="font-extralight">
+              <span className="font-extraligh pocket:text-sm">
                 O caminho padrão que todos são ensinados a começar no
                 desenvolvimento web é com HTML/ CSS, depois partir para o
                 JavaScript. Essa é a única forma?
               </span>
             </div>
           </Grid>
-          <Grid item xs={3}>
-            <FiDatabase className="w-40 h-28 object-cover ml-8 rounded-md mb-3 relative text-white" />
+          <Grid item xs={3} className="tablet:pt-10 phone:pt-12">
+            <FiDatabase  className="w-40 h-28 object-cover ml-8 rounded-md mb-3 relative text-white tablet:w-28 tablet:h-16 phone:w-28 phone:h-16 pocket:w-20 pocket:h-12 tablet:ml-0 phone:ml-0 pocket:ml-0 pocket:mt-24" />
           </Grid>
           <Grid item xs={9}>
             <div className="text-white flex flex-col justify-between pt-5 relative">
@@ -80,11 +80,11 @@ export const Blog = () => {
                 to="https://raulbattistini.hashnode.dev/series/databases"
                 className="font-thin"
               >
-                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-sm hover:text-yellow-500">
+                <h4 className="text-white flex flex-col justify-between font-light bg-gradient-to-r from-indigo-500 to-red-500 pl-3 rounded-l-sm hover:text-yellow-500">
                   Construí todo o projeto, e agora? Onde armazenar?
                 </h4>
               </Link>
-              <span className="font-extralight">
+              <span className="font-extralight pocket:text-sm">
                 Se não houvesse onde se armazenar dados, a web seria um
                 arquipélago de sites estáticos, se é que teria existido. Graças
                 aos bancos de dados, não é assim que a web funciona. E qual
